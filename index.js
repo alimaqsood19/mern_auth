0f565fb68749aefa83db33d17f241dec12fab731
alimaqsood19 <ali-maqsood@Hotmail.com>
2017-07-31 16:57:08 -0400

    Server setup

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Main starting point of application
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app); //Create an HTTP server that recives requests, that forwards it to our application
+
+//App Setup
+
+//Middleware
+app.use(morgan('combined'));//Logging framework, logging incomming requests used for debugging etc
+app.use(bodyParser.json({type: '*/*'}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+//Server Setup
+server.listen(port, () => {
+    console.log(`Server listening on: ${port}`);
+});
